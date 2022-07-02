e56290136f2f52f3c1956633ad813177c83f9953
Andrei Karpenko <andrei.karpenko@telekom.com>
2022-07-02 17:54:45 +0000

    Finish custom template tags

--- blog/templatetags/blog_extras.py
+++ blog/templatetags/blog_extras.py
@@ -3,11 +3,42 @@ from django import template
 #from django.utils.html import escape
 #from django.utils.safestring import mark_safe
 from django.utils.html import format_html
+from blog.models import Post
 
 user_model = get_user_model()
 register = template.Library()
 
+'''
+#access template context
+@register.simple_tag(takes_context=True)
+def author_details_tag(context):
+    request = context["request"]
+    current_user = request.user
+    post = context["post"]
+    author = post.author
 
+    if author == current_user:
+        return format_html("<strong>me</strong>")
+
+    if author.first_name and author.last_name:
+        name = f"{author.first_name} {author.last_name}"
+    else:
+        name = f"{author.username}"
+
+    if author.email:
+        prefix = format_html('<a href="mailto:{}">', author.email)
+        suffix = format_html("</a>")
+    else:
+        prefix = ""
+        suffix = ""
+
+    return format_html("{}{}{}", prefix, name, suffix)
+
+And in template:
+<small>By {% author_details_tag %} on {{ post.published_at|date:"M, d Y" }}</small>
+'''
+
+#without accessing template context
 @register.filter#(name="author_details") - to customize name of the func
 def author_details(author, current_user=None):
   if not isinstance(author, user_model):
@@ -31,6 +62,31 @@ def author_details(author, current_user=None):
       suffix = ""
 
   #except format_html, there can be mark_safe - later injected html will be interpreted as html, but not a text
-  #format_html works 1) as str.format and escape every var 2) mark_safe == format_html 
+  #format_html works 1) as str.format and escape every var 2) "mark_safe" with str argument  == format_html with str argument
   return format_html('{}{}{}', prefix, name, suffix) 
-        
+      
+
+@register.simple_tag#(name="author_details") - to customize name of the func
+def row(extra_classes=""):
+    return format_html('<div class="row {}">', extra_classes)
+
+
+@register.simple_tag#(name="author_details") - to customize name of the func
+def endrow():
+    return format_html('</div>')
+
+
+@register.simple_tag#(name="author_details") - to customize name of the func
+def col(extra_classes=""):
+    return format_html('<div class="col {}">', extra_classes)
+
+
+@register.simple_tag#(name="author_details") - to customize name of the func
+def endcol():
+    return format_html('</div>')
+
+
+@register.inclusion_tag("blog/post-list.html")
+def recent_posts(post):
+    posts = Post.objects.exclude(pk=post.pk)[:2]
+    return {"title": "Recent Posts", "posts": posts}
--- blog/urls.py
+++ blog/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from blog.views import *
 
 urlpatterns = [
-    path('', index, name='index')
+    path('', index, name='index'),
+    path('post/<slug>/', post_detail, name="blog-post-detail"),
 ]
